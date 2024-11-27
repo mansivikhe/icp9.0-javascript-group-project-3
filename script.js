3ab71dfab304d9f26e7fb111f1b6dad84e854fdc
arbajjamadar46 <arbajjamadar46@gmail.com>
2024-11-27 23:48:35 +0530

    Changed the directory of HTML

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-// to fetch Navbar and footer
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('navbar').innerHTML = data);
+// Inject the navbar dynamically
+document.getElementById("navbar").innerHTML = `
+    <div class="navbar-container">
+        <img src="../img/bombay.png" alt="Logo" class="navbar-logo">
+        <div class="navbar-links">
+            <a href="../index.html">Home</a>
+            <a href="/pages/menu.html">Menu</a>
+            <a href="/pages/about.html">About Us</a>
+            <a href="/pages/order.html">Order Online</a>
+            <a href="/pages/reviews.html">Reviews</a>
+            <a href="/pages/events.html">Events</a>
+            <a href="/pages/reservation.html">Reservation</a>
+        </div>
+    </div>  
+`;
 
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('footer').innerHTML = data);
-});
+// Inject the footer dynamically
+document.getElementById("footer").innerHTML = `
+    <footer class="footer">
+        <p>&copy; 2024 Food Restaurant. All rights reserved.</p>
+    </footer>
+`;
+
 
 // Typewriter effect
 const text = "Welcome to HOT N' FAST Restaurant";
