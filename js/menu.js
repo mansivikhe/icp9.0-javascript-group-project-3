1283b1987afaac1400d6905b6626396c645964f3
Amruta <amrutabmaskar@gmail.com>
2024-11-30 17:36:07 +0530

    add onclick event for menus

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,42 @@ openBtn.addEventListener("click", () => {
     sidebar.style.width = "0"; 
     openBtn.style.display = "block"; 
   });
+
+  //search image
+  function filterMenu(category) {
+    const menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach(item => {
+        if (category === 'all' || item.classList.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+  }
+  
+  
+  document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = this.getAttribute('href');
+        document.querySelector(target).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
+  
+  function searchMenu() {
+    const searchInput = document.getElementById('searchBar').value.toLowerCase();
+    const menuItems = document.querySelectorAll('.menu-item');
+  
+    menuItems.forEach(item => {
+        const itemName = item.querySelector('h2').textContent.toLowerCase();
+        if (itemName.includes(searchInput)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+  }
+  
+  
