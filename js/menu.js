3453d10a0ba8b8b0edbc2ffb56f50ecde9292b33
Amruta <amrutabmaskar@gmail.com>
2024-11-30 11:56:06 +0530

    add javascript to open sidebar

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,8 @@
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 const sidebar = document.getElementById("sidebar");
+//open sidebar
+openBtn.addEventListener("click", () => {
+    sidebar.style.width = "250px"; 
+    openBtn.style.display = "none";
+  });
