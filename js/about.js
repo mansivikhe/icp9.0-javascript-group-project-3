f90c1638486d535578b07f6e9a60242ac507d2bb
Disha-Nalawade <dishapatil8525@gmail.com>
2024-12-01 15:54:50 +0530

    slidebar images added

--- js/about.js
+++ js/about.js
@@ -1,32 +1,39 @@
-const sidebarelement = document.getElementById("sidebar");
-function opensidebar() {
-  sidebarelement.style.visibility = "visible";
-}
-function close() {
-  sidebarelement.style.visibility = "hiddedn";
-}
-const imgArray = [
-  "../img/christmas.jpg",
-  "../img/hygin.webp",
-  "./img/buger.jpg",
-  "./img/img3.jpg",
-  
-];
-let index = 0;
-const slide = document.getElementById("slide");
-function next() {
-  if (index < imgArray.length - 1) {
-    index++;
-  } else {
-    index = 0;
+// const sidebarelement = document.getElementById("sidebar");
+// function opensidebar() {
+//   sidebarelement.style.visibility = "visible";
+// }
+// function close() {
+//   sidebarelement.style.visibility = "hiddedn";
+// }
+document.addEventListener("DOMContentLoaded", () => {
+  const imgArray1 = [
+    "../img/christmas.jpg",
+    "../img/hygin.jpg",
+    "../img/buger.jpg",
+    "../img/img3.jpg",
+  ];
+  let index = 0;
+  const slide1 = document.getElementById("slide1");
+
+  function next1() {
+    if (index < imgArray1.length - 1) {
+      index++;
+    } else {
+      index = 0;
+    }
+    slide1.src = imgArray1[index];
   }
-  slide.src = imgArray[index];
-}
-function back() {
-  if (index > 0) {
-    index--;
-  } else {
-    index = imgArray.length - 1;
+
+  function back1() {
+    if (index > 0) {
+      index--;
+    } else {
+      index = imgArray1.length - 1;
+    }
+    slide1.src = imgArray1[index];
   }
-  slide.src = imgArray[index];
-}
+
+  // Attach functions to global scope if using inline event handlers
+  window.next1 = next1;
+  window.back1 = back1;
+});
