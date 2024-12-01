eab8fa7a8cf9bdd1165f8bae1ce9bfe84b86979e
Disha-Nalawade <dishapatil8525@gmail.com>
2024-12-01 16:02:59 +0530

    contact added on bottom fixed

--- js/about.js
+++ js/about.js
@@ -5,35 +5,36 @@
 // function close() {
 //   sidebarelement.style.visibility = "hiddedn";
 // }
-document.addEventListener("DOMContentLoaded", () => {
-  const imgArray1 = [
-    "../img/christmas.jpg",
-    "../img/hygin.jpg",
-    "../img/buger.jpg",
-    "../img/img3.jpg",
-  ];
-  let index = 0;
-  const slide1 = document.getElementById("slide1");
+// document.addEventListener("DOMContentLoaded", () => {
+const imgArray1 = [
+  "../img/restaurant-hall",
+  "../img/christmas.jpg",
+  "../img/hygin.jpg",
+  "../img/buger.jpg",
+  "../img/img3.jpg",
+];
+let index = 0;
+const slide1 = document.getElementById("slide1");
 
-  function next1() {
-    if (index < imgArray1.length - 1) {
-      index++;
-    } else {
-      index = 0;
-    }
-    slide1.src = imgArray1[index];
+function next1() {
+  if (index < imgArray1.length - 1) {
+    index++;
+  } else {
+    index = 0;
   }
+  slide1.src = imgArray1[index];
+}
 
-  function back1() {
-    if (index > 0) {
-      index--;
-    } else {
-      index = imgArray1.length - 1;
-    }
-    slide1.src = imgArray1[index];
+function back1() {
+  if (index > 0) {
+    index--;
+  } else {
+    index = imgArray1.length - 1;
   }
+  slide1.src = imgArray1[index];
+}
 
-  // Attach functions to global scope if using inline event handlers
-  window.next1 = next1;
-  window.back1 = back1;
-});
+//   // Attach functions to global scope if using inline event handlers
+//   window.next1 = next1;
+//   window.back1 = back1;
+// });
