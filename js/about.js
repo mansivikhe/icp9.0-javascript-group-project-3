3ced4a12024d5d98553940754ea80daf6a0042af
Disha-Nalawade <dishapatil8525@gmail.com>
2024-12-01 13:24:51 +0530

    back and previous arrow added

--- js/about.js
+++ js/about.js
@@ -5,3 +5,28 @@ function opensidebar() {
 function close() {
   sidebarelement.style.visibility = "hiddedn";
 }
+const imgArray = [
+  "../img/christmas.jpg",
+  "../img/hygin.webp",
+  "./img/buger.jpg",
+  "./img/img3.jpg",
+  
+];
+let index = 0;
+const slide = document.getElementById("slide");
+function next() {
+  if (index < imgArray.length - 1) {
+    index++;
+  } else {
+    index = 0;
+  }
+  slide.src = imgArray[index];
+}
+function back() {
+  if (index > 0) {
+    index--;
+  } else {
+    index = imgArray.length - 1;
+  }
+  slide.src = imgArray[index];
+}
