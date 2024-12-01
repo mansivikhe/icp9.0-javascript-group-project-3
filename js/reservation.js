dc773a7966fddef83a54c986551633ada84a25c6
Namratawaje20 <wajenamrata123@gmail.com>
2024-12-01 09:35:45 +0530

    alert mess added in js

--- js/reservation.js
+++ js/reservation.js
@@ -1,3 +1,4 @@
 document.querySelector(".reservation-form").addEventListener("submit", function (e) {
     e.preventDefault();
+    alert("🎉 Your reservation has been confirmed! We can't wait to see you soon. 😊");
 });
