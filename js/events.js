940ce85723648830f8e3f99da3e3f4d2593fddb4
YogitaBhujadi <yogitabhujadi6@gmail.com>
2024-12-03 20:18:26 +0530

    css add

--- js/events.js
+++ js/events.js
@@ -20,5 +20,31 @@ function closeModel() {
 }
 
 
+let slider = document.querySelector('.slider');
+let list = document.querySelector('.list');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let items = document.querySelectorAll('.list .item');
+let count = items.length;
+let active = 0; 
 
+let width_item = items[0].offsetWidth;
+
+
+next.onclick = () => {
+    active = active >= count - 1 ? 0 : active + 1;
+    runCarousel();
+}
+
+
+prev.onclick = () => {
+    active = active <= 0 ? count - 1 : active - 1;
+    runCarousel();
+}
+
+function runCarousel() {
+    
+    prev.style.display = (active == 0) ? 'none' : 'block';
+    next.style.display = (active == count - 1) ? 'none' : 'block';
+}
      
