32ac520206bce11c07144f5fda721e797f12c4c0
arbajjamadar46 <arbajjamadar46@gmail.com>
2024-12-03 19:11:34 +0530

    Logo icon changed

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ document.getElementById("navbar").innerHTML = `
             <a href="/pages/about.html">About Us</a>
             <a href="/pages/offers.html">Offers</a>
             <a href="/pages/order.html">Order Online</a>
-            <a href="/pages/reviews.html">Reviews</a>
             <a href="/pages/events.html">Events</a>
             <a href="/pages/reservation.html">Reservation</a>
         </div>
@@ -42,16 +41,16 @@ document.getElementById("footer").innerHTML = `
             <div class="footer-coonect-us-container">
                 <h2 class="features-heading font-family">Conncet with Us</h2>
                     <div class="fb-container">
-                        <a href="https://www.facebook.com/nurserylive/" style="text-decoration: none;">
+                        <a href="https://www.facebook.com/" style="text-decoration: none;">
                             <img src="/img/facebook.png" alt="facebook" class="fb">
                         </a>
-                        <a href="https://chat.whatsapp.com/DSuYvFXc0qH6MOAXTho5nS" style="text-decoration: none;"><img
+                        <a href="https://chat.whatsapp.com/" style="text-decoration: none;"><img
                                 src="/img/whatsapp.png" alt="whatsapp" class="fb">
                         </a>
-                        <a href="https://www.youtube.com/watch?v=Xmg2Hyv7Pmg" style="text-decoration: none;"><img
+                        <a href="https://www.youtube.com/" style="text-decoration: none;"><img
                                 src="/img/youtube.png" alt="youtube" class="fb">
                         </a>
-                        <a href="https://www.instagram.com/nurserylive_/?hl=en" style="text-decoration: none;">
+                        <a href="https://www.instagram.com/" style="text-decoration: none;">
                             <img src="/img/instagram.png" alt="instagram" class="fb">
                         </a>
                     </div>
@@ -59,7 +58,7 @@ document.getElementById("footer").innerHTML = `
 
             </div>
             <div class="map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d500.0307940025006!2d73.79416121326348!3d18.598850116478292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b91e0701ce81%3A0x6499f7f8d8cdf224!2sLotus%20Multi-specialty%20Hospital!5e1!3m2!1sen!2sin!4v1733145258212!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d500.0307940025006!2d73.79416121326348!3d18.598850116478292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b91e0701ce81%3A0x6499f7f8d8cdf224!2sLotus%20Multi-specialty%20Hospital!5e1!3m2!1sen!2sin!4v1733145258212!5m2!1sen!2sin" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
         </div>
     <footer class="footer">
@@ -69,7 +68,7 @@ document.getElementById("footer").innerHTML = `
 
 
 // Typewriter effect
-const text = "Welcome to HOT N' FAST Restaurant";
+const text = "HOT N' FAST Restaurant";
 let i = 0;
 const typingSpeed = 100;
 function typeWriter() {
