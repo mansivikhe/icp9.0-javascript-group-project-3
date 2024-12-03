567b900222dd27ac6f442461906c162ff7566737
YogitaBhujadi <yogitabhujadi6@gmail.com>
2024-12-03 09:28:04 +0530

    css add

--- js/events.js
+++ js/events.js
@@ -7,3 +7,13 @@ function closeModel(){
     const overlayElement = document.getElementById('overlay');
     overlayElement.style.display= 'none';
 }
+
+function openModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display= 'flex';
+}
+
+function closeModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display= 'none';
+}
