2fbb37debe3c7c206e7f8559bbeaaffa977c7985
YogitaBhujadi <yogitabhujadi6@gmail.com>
2024-12-03 10:41:19 +0530

    js add

--- js/events.js
+++ js/events.js
@@ -1,19 +1,20 @@
-function openModel(){
-    const overlayElement = document.getElementById('overlay');
-    overlayElement.style.display= 'flex';
-}
 
-function closeModel(){
-    const overlayElement = document.getElementById('overlay');
-    overlayElement.style.display= 'none';
-}
-
-function openModal(){
+function openModal() {
     const divisionElement = document.getElementById('division');
-    divisionElement.style.display= 'flex';
+    divisionElement.style.display = 'flex';
 }
 
-function closeModal(){
-    const overlayElement = document.getElementById('division');
-    divisionElement.style.display= 'none';
+function closeModal() {
+    const divisionElement = document.getElementById('division');
+    divisionElement.style.display = 'none'; 
+}
+
+function openModel() {
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'flex';
+}
+
+function closeModel() {
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'none';
 }
